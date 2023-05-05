5963737d24c1fa6e73fb70a4298ba6d404093f16
edwardcastle <eduardo.castillo@teavaro.com>
2023-05-04 23:24:32 -0400

    refactor: Update templates path.

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/', views.home, name='room')
+    path('room/<str:pk>/', views.home, name='room')
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'}
+]
 
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 
-def rooms(request):
-    return HttpResponse('Room')
+def room(request):
+    return render(request, 'base/room.html')
